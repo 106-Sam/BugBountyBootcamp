69082573835fe8f08967933ab826446df794a005
1o6-sam <samiabdul916@gmail.com>
2026-04-30 08:00:38 -0400

    Added date to the script

--- recon.sh
+++ recon.sh
@@ -5,16 +5,21 @@ DOMAIN=$1
 DIRECTORY=${DOMAIN}_recon
 PATH_TO_DIRSEARCH=~/dirsearch
 
+
+TODAY=$(date)
+echo "This scan was created on $TODAY"
+
 #Directory Creation
 echo "Creating directory $DIRECTORY"
 mkdir $DIRECTORY
 
 #Nmap Scan on the directory
 nmap -sC -sV $DOMAIN > $DIRECTORY/nmap
-echo "Saved the Nmap results:$DIRECTORY/nmap"
+echo "Saved the Nmap results: $DIRECTORY/nmap"
 
 #Dirsearch on the domain
 
 source $PATH_TO_DIRSEARCH/venv/bin/activate
-python3 $PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php -o $DIRECTORY/dirsearch 
+python3 $PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php -o $DIRECTORY/dirsearch
+echo "Saved the Dirsearch results: $DIRECTORY/dirsearch"
 deactivate
