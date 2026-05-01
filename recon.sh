c302b4661515e2b337f47011ff9c32ee46f5111a
1o6-sam <samiabdul916@gmail.com>
2026-05-01 05:01:39 -0400

    fixed conditional errors

--- recon.sh
+++ recon.sh
@@ -11,15 +11,30 @@ echo "This scan was created on $TODAY"
 
 #Directory Creation
 echo "Creating directory $DIRECTORY"
-mkdir $DIRECTORY
+mkdir -p $DIRECTORY
 
-#Nmap Scan on the directory
-nmap -sC -sV $DOMAIN > $DIRECTORY/nmap
-echo "Saved the Nmap results: $DIRECTORY/nmap"
+if [ "$2" == "nmap-only" ]
+then
+	nmap -sC -sV $DOMAIN > $DIRECTORY/nmap
+	echo "Saved the Nmap results: $DIRECTORY/nmap"
 
-#Dirsearch on the domain
+elif [ "$2" == "dirsearch-only" ]
+then
+	source $PATH_TO_DIRSEARCH/venv/bin/activate
+	python3 $PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php -o $DIRECTORY/dirsearch
+	echo "Saved the Dirsearch results: $DIRECTORY/dirsearch"
+	deactivate
 
-source $PATH_TO_DIRSEARCH/venv/bin/activate
-python3 $PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php -o $DIRECTORY/dirsearch
-echo "Saved the Dirsearch results: $DIRECTORY/dirsearch"
-deactivate
+else
+	
+	#Nmap Scan on the directory
+	nmap -sC -sV $DOMAIN > $DIRECTORY/nmap
+	echo "Saved the Nmap results: $DIRECTORY/nmap"
+
+	#Dirsearch on the domain
+	source $PATH_TO_DIRSEARCH/venv/bin/activate
+	python3 $PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php -o $DIRECTORY/dirsearch
+	echo "Saved the Dirsearch results: $DIRECTORY/dirsearch"
+	deactivate
+fi
+
