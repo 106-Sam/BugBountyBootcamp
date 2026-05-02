02642c33f9dafe31bcf3255fe44d4028ea9414d4
1o6-sam <samiabdul916@gmail.com>
2026-05-02 15:37:39 -0400

    Added Report parsing and results in one file

--- recon-function.sh
+++ recon-function.sh
@@ -7,25 +7,30 @@ PATH_TO_DIRSEARCH=~/dirsearch
 
 
 TODAY=$(date)
-echo "This scan was created on $TODAY"
 
 #Directory Creation
 echo "Creating directory $DIRECTORY"
 mkdir -p $DIRECTORY
+echo -e "This scan was created on $TODAY\n"> $DIRECTORY/results
 
 nmap_scan(){
-	nmap -sC -sV $DOMAIN > $DIRECTORY/nmap
-	echo "saved the Nmap results: $DIRECTORY/nmap."
+	nmap $DOMAIN > $DIRECTORY/nmap
+	echo -e "Results for Nmap:\n" >> $DIRECTORY/results
+	grep -E "^\S+\s+\S+\s+\S*$" $DIRECTORY/nmap >> $DIRECTORY/results
 }
 dirsearch_scan(){
 	source $PATH_TO_DIRSEARCH/venv/bin/activate
 	python3 $PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php -o $DIRECTORY/dirsearch
         echo "Saved the Dirsearch results: $DIRECTORY/dirsearch."
+	echo -e "Results for Dirsearch:\n" >> $DIRECTORY/results
+	cat $DIRECTORY/dirsearch >> $DIRECTORY/results
        	deactivate
 }
 crt_scan(){
-	curl "https://crt.sh/?q=$DOMAIN&output=json" -o $DIRECTORY/crt
-	echo "Saved the cert parsing results: $DIRECTORY/crt."		
+	curl -o $DIRECTORY/crt "https://crt.sh/?q=$DOMAIN&output=json"
+	echo -e "\nResults for cert parsing:\n" >> $DIRECTORY/results
+	echo "Saved the cert parsing results: $DIRECTORY/crt."
+	jq -r ".[] | .name_value" $DIRECTORY/crt >> $DIRECTORY/results
 }
 
 case $2 in
